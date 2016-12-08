9d88599f8e33af0aa8d089e805c6bb42fe4a7393
jlyon87 <jml6487@gmail.com>
2016-12-07 19:11:46 -0500

    port config cleanup

--- app/server.js
+++ app/server.js
@@ -4,14 +4,16 @@ const app = express();
 const path = require("path");
 const router = require("./router");
 
-//app.set('port', (process.env.PORT || 5000));
+
 
 app.use('/', router);
 
-/*app.listen(app.get('port', function() {
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port', function() {
 	console.log("Node App listening on port:", app.get('port'));
-}));*/
+}));
+
 
 console.log(process.env.PORT);
-app.listen(process.env.PORT || 5000);
-console.log("Server.js is listening on port: 5000.");
+/*app.listen(process.env.PORT || 5000);
+console.log("Server.js is listening on port: 5000.");*/
