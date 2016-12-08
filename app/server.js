0de381807fe1df8ade16063c99eac1c4dd2d60e7
jlyon87 <jml6487@gmail.com>
2016-12-07 19:18:17 -0500

    final port config

--- app/server.js
+++ app/server.js
@@ -4,19 +4,9 @@ const app = express();
 const path = require("path");
 const router = require("./router");
 
-//app.set('port', (process.env.PORT || 5000));
-
 app.use('/', router);
 
-console.log(process.env.PORT);
-console.log('before port setter');
 app.set('port', (process.env.PORT || 5000));
-console.log('after port setter');
 app.listen(app.get('port'), function() {
 	console.log("Node App listening on port:", app.get('port'));
 });
-
-
-console.log(process.env.PORT);
-/*app.listen(process.env.PORT || 5000);
-console.log("Server.js is listening on port: 5000.");*/
