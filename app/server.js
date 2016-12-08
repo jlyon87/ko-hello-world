3dca145894d8ac3e44c1df3dca3a3f83950ecefb
jlyon87 <jml6487@gmail.com>
2016-12-07 19:14:53 -0500

    port config change.

--- app/server.js
+++ app/server.js
@@ -4,14 +4,17 @@ const app = express();
 const path = require("path");
 const router = require("./router");
 
-
+//app.set('port', (process.env.PORT || 5000));
 
 app.use('/', router);
 
+console.log(process.env.PORT);
+console.log('before port setter');
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port', function() {
+console.log('after port setter');
+app.listen(app.get('port'), function() {
 	console.log("Node App listening on port:", app.get('port'));
-}));
+});
 
 
 console.log(process.env.PORT);
